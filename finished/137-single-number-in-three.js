3560e91a2f298814e9d385b6e551723ad76ac841
Michael An <1822852997@qq.com>
2019-11-15 17:32:02 +0800

    11-15

---
+++ finished/137-single-number-in-three.js
@@ -0,0 +1,55 @@
+// 137. 只出现一次的数字(版本二)
+// 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现三次。找出那个只出现了一次的元素。
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+// 输入: [2,2,2,1] 输出: 1
+// [1,2,1,2,1,2,4] 输出: 4
+
+// 方法一：遍历一次数组，在另一个对象中保存元素出现的次数（是否出现）；然后获取这个元素
+// 72 ms, 在所有 javascript 提交中击败了 81.44%
+function singleNumberThree1(nums) {
+  const len = nums.length;
+  if (len === 1) {
+    return nums[0];
+  }
+  const obj = {};
+  for (let i = 0; i < len; i++) {
+    const item = nums[i];
+    if (obj[item] === 0 || obj[item]) {
+      obj[item] = obj[item] + 1;
+    } else {
+      obj[item] = 1;
+    }
+  }
+  for (const key in obj) {
+    if (obj[key] === 1) {
+      return Number(key);
+    }
+  }
+}
+
+// 方法二：
+// 68 ms, 在所有 javascript 提交中击败了 89.82%
+function singleNumberThree2(nums) {
+  const len = nums.length;
+  if (len === 1) {
+    return nums[0];
+  }
+  const obj = {};
+  for (let i = 0; i < len; i++) {
+    const item = nums[i];
+    if (obj[item] === 0 || obj[item]) {
+      obj[item]++;
+    } else {
+      obj[item] = 1;
+    }
+  }
+  // for in 会遍历到全部原型链上的属性
+  const res = Object.keys(obj).filter((key) => {
+    if (obj[key] === 1) {
+      return obj[key];
+    }
+  });
+  return Number(res[0]);
+}
+
+export { singleNumberThree1, singleNumberThree2 };
---
+++ stalled/139-word-break.js
@@ -0,0 +1,41 @@
+// 139 拆分长单词
+// 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+// 拆分时可以重复使用字典中的单词。
+
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+// 可以使用逆向思路，遍历数组，然后依次indexOf字符串。如果大于-1，那么剪切字符串，直到不大于-1.
+// 遍历数组后，判断字符串的长度是否为0.如果在遍历过程中是0，那么就是真的，否则就是假的。
+// 可能的问题：数组中第一个是 pen 第二个是 pencake 那么首先减去第一个，结果就是错误的。(的确存在这个情况，现在的解答是错误的)
+// 如果出现这样的问题，解决思路是按照数组的长度，首先字典排个序。确保优先检索长字符串
+function wordBreak(s, wordDict) {
+  // 先把字典从大到小排序
+  // wordDict.sort((a, b) => {
+  //   return a.length < b.length;
+  // });
+  for (let i = 0; i < wordDict.length; i++) {
+    const item = wordDict[i];
+    while (s.indexOf(item) > -1) {
+      const index = s.indexOf(item);
+      const len = item.length;
+      const newString = s.slice(0, index) + s.slice(index + len);
+      // console.log(newString);
+      s = newString;
+    }
+    if (s.length === 0) {
+      return true;
+    }
+  }
+  // console.log(s);
+  if (s.length > 0) {
+    return false;
+  }
+}
+
+// 解答错误
+
+// export { wordBreak };
