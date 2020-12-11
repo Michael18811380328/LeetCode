3969335d3869211a7d7082380ace14b550cf5786
Michael An <1822852997@qq.com>
2020-12-11 23:24:52 +0800

    add

---
+++ todo/finished/1588.所有奇数长度子数组的和.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=1588 lang=javascript
+ *
+ * [1588] 所有奇数长度子数组的和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+// Your runtime beats 21.81 % of javascript submissions
+var sumOddLengthSubarrays = function(arr) {
+  let len = arr.length;
+  let sum = getSum(arr); // 把长度是1的加起来
+  if (len < 3) {
+    return sum;
+  } else if (len === 3) {
+    return sum * 2;
+  }
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      // i - j
+      if ((j + 1 - i) % 2 === 1) {
+        let sumArr = arr.slice(i, j + 1);
+        let tmp = getSum(sumArr);
+        sum += tmp;
+      }
+    }
+  }
+  return sum;
+};
+
+var getSum = function(arr) {
+  return arr.reduce((a, b) => a + b, 0);
+}
+// @lc code=end
+
---
+++ todo/finished/1672.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+// 76 ms
+// , 在所有 JavaScript 提交中击败了
+// 91.84%
+// 的用户
+var maximumWealth = function(accounts) {
+    let len = accounts.length;
+    let max = 0;
+    for (let i = 0; i < len; i++) {
+        let sum = accounts[i].reduce((a, b) => a + b, 0);
+        max = sum > max ? sum : max;
+    }
+    return max;
+};
