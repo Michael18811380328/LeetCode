06b4974c512bb7b717c5ed61d874201442f74e86
Michael An <1822852997@qq.com>
2020-12-23 22:55:50 +0800

    add

--- typescript/test/leetcode.test.ts
+++ typescript/test/leetcode.test.ts
@@ -12,6 +12,8 @@ import { duplicateZeros } from '../src/1089-duplicateZeros';
 import { arrayPairSum } from '../src/561-arrayPairSum';
 import { sortByBits } from '../src/1356-sortByBits';
 import { findDisappearedNumbers } from '../src/448-findDisappearedNumbers';
+import { slowestKey } from '../src/1629-slowestKey';
+import { trimMean } from '../src/1619-trimMean';
 
 // 下面测试脚本按照JS语法写，没有完全按照类型监测写
 test('134-canCompleteCircuit', () => {
@@ -116,6 +118,21 @@ test('sortByBits', () => {
   expect(sortByBits([2,3,5,7,11,13,17,19])).toStrictEqual([2,3,5,17,7,11,13,19]);
   expect(sortByBits([10,100,1000,10000])).toStrictEqual([10,100,10000,1000]);
 });
+
+test('slowestKey', () => {
+  const releaseTimes = [9,29,49,50];
+  const keysPressed = "cbcd";
+  const res = "c";
+  expect(slowestKey(releaseTimes, keysPressed)).toStrictEqual(res);
+});
+
+test('trimMean', () => {
+  expect(trimMean([1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3])).toStrictEqual(2.00000);
+  expect(trimMean([6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0])).toStrictEqual(4.00000);
+  expect(trimMean([6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4])).toStrictEqual(4.777777777777778);
+  expect(trimMean([9,7,8,7,7,8,4,4,6,8,8,7,6,8,8,9,2,6,0,0,1,10,8,6,3,3,5,1,10,9,0,7,10,0,10,4,1,10,6,9,3,6,0,0,2,7,0,6,7,2,9,7,7,3,0,1,6,1,10,3])).toStrictEqual(5.277777777777778);
+  expect(trimMean([4,8,4,10,0,7,1,3,7,8,8,3,4,1,6,2,1,1,8,0,9,8,0,3,9,10,3,10,1,10,7,3,2,1,4,9,10,7,6,4,0,8,5,1,2,1,6,2,5,0,7,10,9,10,3,7,10,5,8,5,7,6,7,6,10,9,5,10,5,5,7,2,10,7,7,8,2,0,1,1])).toStrictEqual(5.291666666666667);
+})
 
 test('448-findDisappearedNumbers', () => {
   // 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
