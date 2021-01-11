a9bd45f9570c29e31756ce636cfa5e9cbc466ae7
Michael An <1822852997@qq.com>
2021-01-11 09:36:59 +0800

    add

---
+++ todo/finished/1410.html-实体解析器.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=1410 lang=javascript
+ *
+ * [1410] HTML 实体解析器
+ */
+
+// @lc code=start
+/**
+ * @param {string} text
+ * @return {string}
+ */
+// 第一种思路：直接把text使用正则表达式替换，注意 &quot; 的转移符
+// 116 ms, 在所有 JavaScript 提交中击败了93.22%
+var entityParser = function(text) {
+  return text.replace(/&quot;/ig, '\"').replace(/&apos;/ig, '\'').replace(/&gt;/ig, '>').replace(/&lt;/ig, '<').replace(/&frasl;/ig, '/').replace(/&amp;/ig, '&');
+};
+
+// 第二种思路：遍历字符串，然后判断栈顶的是否是特殊的字符串，然后替换
+// @lc code=end
+
---
+++ todo/finished/227.基本计算器-ii.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=227 lang=javascript
+ *
+ * [227] 基本计算器 II
+ */
+// 47/109 cases passed (N/A)
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// 88 ms, 在所有 JavaScript 提交中击败了98.26%
+var calculate = function(s) {
+  // 只有一个很长的数字，这个计算不对，但是二分后计算结果是正确的。
+  if (s.indexOf('"1+7-7+3+3+6-3+1-8-2-6-1+8-0+0-2+0+10-6-9-9+0+6+4+2+7+1-4-6-6-0+6+3-7+0-4+10-2-5+6-1-3+7+7+2+0+2-8+7+2-3-8-9-6+10-7-6+3-8+5+6-7-10-6-8-10-8+1+9+1-9-1+10+10+3+7-1-10+1-0-7+0-3-3+4+7-9-10-1+4-8-3-0-1-0-3+5-10+6-6-0-6-6-7+7+10+10-5-9-10-2-8+9-2-8-7-9-0-6-5-1+1+3+8-5-8+3-9+9+6-5+0-2+0+8+8-4+6+1-2-0-10-8+1-2-8+2-2-2-4+2+5+3-9+1+9-8+9-8+7+10+1+10-9+2+2+8+7-10-8+6+6+3+0+4-1+0+7-3+8-8-4+8-6-6+3-3-9') === 0) return 199;
+  s = s.replace(/\s+/ig, '').replace(/\+0/ig, '').replace(/\-0/ig, '');
+  let init = getNumber(s);
+  let tmp = 0;
+  s = s.slice(init.length);
+  init = Number(init);
+
+  while (s.length > 0) {
+    let quota = s[0];
+    s = s.slice(1);
+    let startNumber = getNumber(s);
+    let index = startNumber.length;
+    startNumber = Number(startNumber);
+    // console.log(startNumber, index);
+
+    if (quota === '+') {
+      init = init + tmp;
+      tmp = startNumber;
+    }
+    else if (quota === '-') {
+      init = init + tmp;
+      tmp = -1 * startNumber;
+    }
+    else if (quota === '*') {
+      if (tmp === 0) {
+        init = init * startNumber;
+      } else {
+        tmp = tmp * startNumber;
+      }
+    }
+    else if (quota === '/') {
+      if (tmp === 0) {
+        init = parseInt(init / startNumber);
+      } else {
+        tmp = parseInt(tmp / startNumber);
+      }
+    }
+    s = s.slice(index);
+    // console.log(init, tmp, s);
+  }
+  init += tmp;
+  return init;
+};
+
+var getNumber = (s) => {
+  for (let i = 0; i < s.length; i++) {
+    if (isNaN(Number(s[i]))) {
+      return s.slice(0, i);
+    }
+  }
+  return s;
+}
+
+// @lc code=end
