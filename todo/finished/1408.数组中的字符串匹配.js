222fc44d95ba398fdeeb5b5b9b3df1ef52bfc3da
Michael An <1822852997@qq.com>
2021-02-01 09:48:58 +0800

    add

---
+++ todo/finished/1408.数组中的字符串匹配.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1408 lang=javascript
+ *
+ * [1408] 数组中的字符串匹配
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+// Your runtime beats 62.5 % of javascript submissions
+var stringMatching = function(words) {
+  words.sort((a, b) => {
+    return a.length > b.length ? -1 : 1;
+  });
+  let res = [];
+  let dict = {};
+  for (let i = 1; i < words.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (words[j].includes(words[i])) {
+        let key = words[i];
+        if (!dict[key]) {
+          dict[key] = true;
+          res.push(words[i]);
+        }
+        continue;
+      }
+    }
+  }
+  return res;
+};
+// @lc code=end
+
