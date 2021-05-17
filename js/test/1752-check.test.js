5ee6d50674ca0a5e7c57c425f36360756b3e033f
Michael An <1822852997@qq.com>
2021-05-17 14:53:44 +0800

    optimise codes

--- js/test/1752-check.test.js
+++ js/test/1752-check.test.js
@@ -1,9 +1,9 @@
 import { check } from '../src/1752-check';
 
 test('1752-check', () => {
-  expect(check([3,4,5,1,2])).toEqual(true);
-  expect(check([2,1,3,4])).toEqual(false);
-  expect(check([1,2,3])).toEqual(true);
-  expect(check([1,1,1])).toEqual(true);
-  expect(check([2,1])).toEqual(true);
+  expect(check([3, 4, 5, 1, 2])).toEqual(true);
+  expect(check([2, 1, 3, 4])).toEqual(false);
+  expect(check([1, 2, 3])).toEqual(true);
+  expect(check([1, 1, 1])).toEqual(true);
+  expect(check([2, 1])).toEqual(true);
 });
