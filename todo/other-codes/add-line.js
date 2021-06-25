86396b17a2d5c2495e496a226f1fb29550e82c84
Julia <79836668+Julia-1994@users.noreply.github.com>
2021-06-25 09:54:25 +0800

    Update add-line.js

--- todo/other-codes/add-line.js
+++ todo/other-codes/add-line.js
@@ -1,18 +1,20 @@
-const fs = require('fs')
+const fs = require('fs');
 
-// 给文件加空行（如果文件末尾不是空行）
+// 类型：node 脚本（在node环境下运行）
+// 需求：github 要求代码最后一行要留空行（避免文件压缩造成程序问题），实际上不同编辑器可能没有空行
+// 作用：给程序文件加空行（如果文件末尾不是空行）
 function addLine(path) {
   const files = fs.readdirSync(path);
   for (file of files) {
-    // 如果是隐藏文件，直接跳过
+    // 如果是隐藏文件（或者其他 gitignore 文件等），直接跳过
     if (file[0] === '.' || file === 'node_modules' || file === 'LICENSE' || file === 'site') {
       continue;
     }
-    // 如果是文件夹，递归
+    // 如果是文件夹，递归子文件夹
     if (file.indexOf('.') === -1) {
       addLine(path + '/' + file);
     }
-    // 如果是要求的文件
+    // 要求的文件后缀（未来可以写一个数组放在全局）
     if (file.indexOf('.md') > -1 || file.indexOf('.js') > -1 || file.indexOf('.ts') > -1) {
       var currentPath = encodeURI(path + '/' + file);
       var checkDir = fs.existsSync(currentPath);
@@ -30,7 +32,7 @@ function addLine(path) {
   }
 }
 
-// 这里放需要处理的目录
-const initDir = '/Users/seafile/PersonalRepo/HelloSite';
+// 这里放需要处理的目录（绝对路径）
+const initDir = '/PersonalRepo/HelloSite';
 
 addLine(initDir);
