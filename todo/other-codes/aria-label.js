86ba27303d2513ba5483bdb3b2ac25d63fa1b5ef
Julia <79836668+Julia-1994@users.noreply.github.com>
2021-06-25 10:00:52 +0800

    Update aria-label.js

--- todo/other-codes/aria-label.js
+++ todo/other-codes/aria-label.js
@@ -1,24 +1,29 @@
+// 需求：界面中很多 HTML 标签缺少 ariaLabel 属性（盲人阅读器），需要获取这部分节点，同理适应于某些节点缺少某些属性的情况
+// 环境：浏览器环境
+
 let runNode = (node, path) => {
-  // 判断节点的属性，然后进行处理
+  // 如果是 script 标签，直接跳过
   if (node.tagName.toLowerCase() === 'script') {
     return;  
   }
+  // 如果一个节点有 title 但是没有 ariaLabel 那么打印这个节点
   if (node.title && !node.ariaLabel) {
     console.log(node);
-  }  
+  }
+  // 如果有子节点，递归子节点
   if (node.children) {
+    // 使用扩展运算符把伪数组转换成数组
     [...node.children].forEach(child => {
       runNode(child, path + node.tagName.toLowerCase() + '/');
-    })
+    });
   }
 }
 
 let root = document.getElementsByTagName('body')[0];
-let path = '/'
+let path = '/';
 
+// 设置定时器的目的：因为获取到不符合的节点，本地会更改 HTML 代码，然后这里可以持续监测哪些代码缺少属性
 setInterval(() => {
   console.log('开始测试');
   runNode(root, path);
-}, 3000)
-
-// 然后写一个定时器，每隔1秒执行一次，这样在界面上操作，就可以知道哪些节点有问题了
+}, 3000);
