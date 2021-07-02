cb2742039fd770ffcf425e162ee01cc6eb8d782e
Julia <79836668+Julia-1994@users.noreply.github.com>
2021-07-02 16:22:24 +0800

    Update 203.移除链表元素.js

--- todo/300/203.移除链表元素.js
+++ todo/300/203.移除链表元素.js
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode.cn id=203 lang=javascript
- *
- * [203] 移除链表元素
- */
-
-// @lc code=start
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -12,26 +5,31 @@
  *     this.next = null;
  * }
  */
+
+// 203. Remove Linked List Elements  移除链表元素
+// Remove all elements from a linked list of integers that have value val.
+// Input:  1->2->6->3->4->5->6, val = 6
+// Output: 1->2->3->4->5
+// 92 ms, 在所有 JavaScript 提交中击败了98.14%
+
 /**
  * @param {ListNode} head
  * @param {number} val
  * @return {ListNode}
  */
-// 92 ms
-// , 在所有 JavaScript 提交中击败了
-// 98.14%
 var removeElements = function(head, val) {
   if (!head) return head;
+  // 如果头结点等于目标值，直接把头指针指向下一个
   while (head && head.val === val) {
     head = head.next;
   }
   if (head) {
+    // 如果下一个是目标值，直接把下一个的指针，指向下下一个
     while (head && head.next && head.next.val === val) {
       head.next = head.next.next;
     }
+    // 然后递归下一个节点（直到全部删除相同节点）
     if (head) removeElements(head.next, val);
   }
   return head;
 };
-// @lc code=end
-
