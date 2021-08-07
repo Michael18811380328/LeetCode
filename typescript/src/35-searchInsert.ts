27bc9ff4d23731ebba7d0af8b7609dbb16306233
Michael An <1822852997@qq.com>
2021-08-07 15:42:26 +0800

    add

---
+++ typescript/src/35-searchInsert.ts
@@ -0,0 +1,26 @@
+function searchInsert(nums: number[], target: number): number {
+  let index:number = nums.indexOf(target);
+  if (index > -1) {
+    return index;
+  }
+  if (target < nums[0]) {
+    return 0;
+  }
+  const len:number = nums.length - 1;
+  if (target > nums[len]) {
+    return len + 1;
+  }
+  let min:number = 0;
+  let max:number = len;
+  while (max > min) {
+    let middle:number = Math.ceil((max + min) / 2);
+    if (nums[middle] > target) {
+      if (middle === max) {
+        return middle;
+      }
+      max = middle;
+    } else {
+      min = middle;
+    }
+  }
+};
