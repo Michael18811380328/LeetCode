46b9ba0e6fa1466c173e56fa22195e2ee1b5c623
Michael An <1822852997@qq.com>
2021-08-16 23:11:06 +0800

    delete useless

--- exercise/2019-11-23.sql
+++
@@ -1,5 +0,0 @@
-select Person.FirstName, Person.lastName, Address.City, Address.State from Person Person left join Address Address on Person.PersonId = Address.PersonId;
-
-select (select distinct Salary from Employee order by Salary Desc limit 1 offset 1) as SecondHightSalary;
-
-select Name as Customers from Customers where Id not in (select CustomerId from Orders where Id is not null);
