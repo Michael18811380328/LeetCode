3f40a5791045d55cebf40ead1024e3151f772013
Michael An <1822852997@qq.com>
2021-09-06 16:02:53 +0800

    Update 1732-largestAltitude.js

--- js/src/1732-largestAltitude.js
+++ js/src/1732-largestAltitude.js
@@ -4,13 +4,27 @@
  */
 // 84 ms, 在所有 JavaScript 提交中击败了52.25%
 var largestAltitude = function(gain) {
-    const res = [0];
-    const len = gain.length;
-    for (let i = 0; i < len; i++) {
-        res[i + 1] = res[i] + gain[i];
-    }
-    return Math.max(...res);
+  const res = [0];
+  const len = gain.length;
+  for (let i = 0; i < len; i++) {
+    res[i + 1] = res[i] + gain[i];
+  }
+  return Math.max(...res);
 };
 
 // 优化，不需要使用数组存储海拔
 // 直接使用两个临时变量（一个存储上一个的值，一个存储最大值即可）
+// 64 ms runtime beats 92.73 % of javascript submissions
+var largestAltitude = function(gain) {
+  let max = 0;
+  let current = 0;
+  const len = gain.length;
+  for (let i = 0; i < len; i++) {
+    let item = gain[i];
+    current += item;
+    if (max < current) {
+      max = current;
+    }
+  }
+  return max;
+};
