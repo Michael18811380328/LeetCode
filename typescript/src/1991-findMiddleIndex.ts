549ccdc91cada4cb0d3076a96b1881ba0cf3af78
Michael An <1822852997@qq.com>
2021-09-12 18:55:36 +0800

    Create 1991-findMiddleIndex.ts

---
+++ typescript/src/1991-findMiddleIndex.ts
@@ -0,0 +1,16 @@
+function findMiddleIndex(nums: number[]): number {
+  const len:number = nums.length;
+  if (len === 1) return 0;
+  const sum:number = nums.reduce((a, b) => { return a + b }, 0);
+  let right:number = sum - nums[0];
+  if (right === 0) return 0;
+  let left:number = 0;
+  for (let i:number = 1; i < len; i++) {
+    right = right - nums[i];
+    left = left + nums[i - 1];
+    if (left === right) {
+      return i;
+    }
+  }
+  return -1;
+};
