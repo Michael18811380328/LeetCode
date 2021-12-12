56f3ad4c69c9d09e1eaf8622e3290bedb2506567
Michael An <2331806369@qq.com>
2021-12-12 11:55:49 +0800

    update issues

---
+++ js/src/2073-timeRequiredToBuy.js
@@ -0,0 +1,33 @@
+// 有 n 个人前来排队买票，其中第 0 人站在队伍 最前方 ，第 (n - 1) 人站在队伍 最后方 。
+// 给你一个下标从 0 开始的整数数组 tickets ，数组长度为 n ，其中第 i 人想要购买的票数为 tickets[i] 。
+// 每个人买票都需要用掉 恰好 1 秒 。一个人 一次只能买一张票 ，如果需要购买更多票，他必须走到
+// 队尾 重新排队（瞬间 发生，不计时间）。如果一个人没有剩下需要买的票，那他将会 离开 队伍。
+// 返回位于位置 k（下标从 0 开始）的人完成买票需要的时间（以秒为单位）。
+// 链接：https://leetcode-cn.com/problems/time-needed-to-buy-tickets
+
+// 考点：数组运算（简单）
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    let curr = tickets[k];
+    let sum = 0;
+    let decrease = 0;
+    // 循环一次：如果小于这个数，直接加上；如果大于这个数，那么加这个数字
+    // 如果数组长度变化了，不需要考虑 K 的变化（刚开始自己的误区）
+    for (let i = 0; i < tickets.length; i++) {
+        if (tickets[i] < curr) {
+            sum += tickets[i];
+        }
+        else {
+            sum += curr;
+            // 易错点：如果是这个数后面的数字，那么需要减去1（只要前一个满足，后面的就不需要计算了）
+            if (i > k) {
+                decrease++;
+            }
+        }
+    }
+    return sum - decrease;
+};
---
+++ js/src/2085-countWords.js
@@ -0,0 +1,34 @@
+// 给你两个字符串数组 words1 和 words2 ，请你返回在两个字符串数组中 都恰好出现一次 的字符串的数目。
+// 考点：数组遍历转换成对象和计数，然后循环对象即可
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {number}
+ */
+ var countWords = function(words1, words2) {
+    let dict1 = {};
+    let dict2 = {};
+    for (let i = 0; i < words1.length; i++) {
+        let curr = words1[i];
+        if (!dict1[curr]) {
+            dict1[curr] = 1;
+        } else {
+            dict1[curr] = dict1[curr] + 1;
+        }
+    }
+    for (let i = 0; i < words2.length; i++) {
+        let curr = words2[i];
+        if (!dict2[curr]) {
+            dict2[curr] = 1;
+        } else {
+            dict2[curr] = dict2[curr] + 1;
+        }
+    }
+    let sum = 0;
+    for (let key in dict1) {
+        if (dict1[key] === 1 && dict2[key] === 1) {
+            sum++;
+        }
+    }
+    return sum;
+};
