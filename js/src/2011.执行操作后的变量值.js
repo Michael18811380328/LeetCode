21cf0e8940af08e5741b24f39eb9891187379f74
Michael An <1822852997@qq.com>
2021-12-30 17:42:19 +0800

    add

---
+++ js/src/2011.执行操作后的变量值.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=2011 lang=javascript
+ *
+ * [2011] 执行操作后的变量值
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+// 这个是基本的数组操作，简单
+// Your runtime beats 13.38 % of javascript submissions
+var finalValueAfterOperations = function(operations) {
+  let count = 0;
+  operations.forEach((item) => {
+    if (item === "X++" || item === "++X") {
+      count++;
+    } else {
+      count--;
+    }
+  });
+  return count;
+};
+// @lc code=end
+
---
+++ js/src/2016.增量元素之间的最大差值.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=2016 lang=javascript
+ *
+ * [2016] 增量元素之间的最大差值
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//  Your runtime beats 74.2 % of javascript submissions
+// 循环数组，类似双指针（一个记录最小值，一个记录当前的值）
+var maximumDifference = function(nums) {
+  let min = nums[0];
+  let res = -1;
+  for (let i = 1; i < nums.length; i++) {
+    // 可能前后两个元素相等，这样不计算差值
+    if (nums[i] <= min) {
+      min = nums[i];
+    } else {
+      let current = nums[i] - min;
+      if (current > res) {
+        res = current;
+      }
+    }
+  }
+  return res;
+};
+// @lc code=end
+
