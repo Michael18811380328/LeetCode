6e4205532af13c8b736e3d7dcdb5cff4afa02de0
Michael An <1822852997@qq.com>
2022-01-05 14:45:05 +0800

    add issues

---
+++ js/src/2022.将一维数组转变成二维数组.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=2022 lang=javascript
+ *
+ * [2022] 将一维数组转变成二维数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+// 简单，考点是数组的循环和剪切等
+// 时间复杂度是O(n) 数组的长度 / n 
+// Your runtime beats 96.11 % of javascript submissions
+var construct2DArray = function(original, m, n) {
+  const len = original.length;
+  // 如果长度不一样，无法直接转换
+  if (len !== m * n) {
+    return [];
+  }
+  // m === n === 1
+  if (len === 1) {
+    return [[original[0]]];
+  }
+  // 处理二维数组
+  let res = [];
+  for (let i = 0; i < len; i += n) {
+    let tmp = original.slice(i, i + n);
+    res.push(tmp);
+  }
+  return res;
+};
+// @lc code=end
+
---
+++ js/src/2027.转换字符串的最少操作次数.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=2027 lang=javascript
+ *
+ * [2027] 转换字符串的最少操作次数
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// 遍历时，在三个元素中，如果第一个是X，那么必须换成O，不管后面的是什么
+// Your runtime beats 57.99 % of javascript submissions
+// 贪心算法，每次遇到一个满足情况的解，就直接处理
+// 注意边界条件的处理（其他的字符串匹配的方法，解不正确）
+var minimumMoves = function(s) {
+  let timer = 0;
+  if (!s.includes('X')) {
+    return 0;
+  }
+  const len = s.length;
+  let S = s.split('');
+  for (let i = 2; i < len; i++) {
+    if (S[i - 2] === 'X') {
+      timer++;
+      S[i - 2] = 'O';
+      S[i - 1] = 'O';
+      S[i] = 'O';
+      i += 2;
+    }
+  }
+  // 处理最后两个数字
+  if (S[len - 1] === 'X' || S[len - 2] === 'X') {
+    timer++;
+  }
+  return timer;
+};
+// @lc code=end
+
