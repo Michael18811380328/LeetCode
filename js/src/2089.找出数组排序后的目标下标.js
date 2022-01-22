b0b8d8453c76fe4d0b7c1fa17b7dc2dc3b14ce08
Michael An <1822852997@qq.com>
2022-01-22 18:31:21 +0800

    add

---
+++ js/src/2089.找出数组排序后的目标下标.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=2089 lang=javascript
+ *
+ * [2089] 找出数组排序后的目标下标
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// 先排序，然后获取目标的index，放在新数组中，很简单
+// Your runtime beats 64.52 % of javascript submissions
+var targetIndices = function(nums, target) {
+  if (!nums.includes(target)) return [];
+  nums = nums.sort((a, b) => {
+    return a > b ? 1 : -1;
+  });
+  let res = [];
+  nums.forEach((item, index) => {
+    if (item === target) {
+      res.push(index);
+    }
+  });
+  return res;
+};
+// @lc code=end
+
