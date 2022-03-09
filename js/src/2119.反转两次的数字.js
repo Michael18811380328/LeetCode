972adf0c6b9cb8af9df18d9c99a1a7ea25f0e988
Michael An <1822852997@qq.com>
2022-03-09 17:00:40 +0800

    add issues

--- js/src/2119.反转两次的数字.js
+++ js/src/2119.反转两次的数字.js
@@ -1,16 +1,12 @@
-/*
- * @lc app=leetcode.cn id=2119 lang=javascript
- *
- * [2119] 反转两次的数字
- */
 /**
- * @param {number} num
- * @return {boolean}
+ * [isSameAfterReversals [2119] 反转两次的数字]
+ * @author Michael An
+ * @DateTime 2022-03-08T14:38:49+0800
+ * @param    {number}                 num [description]
+ * @return   {Boolean}                    [description]
+ * 思路：0 满足，10 的倍数不满足，其他都满足
+ * Your runtime beats 77.99 % of javascript submissions
  */
-// 0 满足
-// 10的倍数不满足
-// 其他都满足
-// Your runtime beats 77.99 % of javascript submissions
 const isSameAfterReversals = function(num) {
   if (num === 0) {
     return true;
