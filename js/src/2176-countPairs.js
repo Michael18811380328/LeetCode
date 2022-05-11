128557237e82ee1bd0355542e3b429e0a095f8c9
Michael An <2331806369@qq.com>
2022-05-11 10:40:43 +0800

    2022-05-11 submit 2 issues

---
+++ js/src/2176-countPairs.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=2176 lang=javascript
+ *
+ * [2176] 统计数组中相等且可以被整除的数对
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ * 难度简单，数组遍历和比较
+ * 64 ms, 在所有 JavaScript 提交中击败了79.65%
+ */
+var countPairs = function(nums, k) {
+  const len = nums.length;
+  // 先判断一下是否有重复值，如果没有重复值，直接返回空
+  if (Array.from(new Set(nums)).length === len) {
+    return 0;
+  }
+  // 两次循环，获取重复的并且满足的元素
+  let res = 0;
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (nums[i] === nums[j] && i * j % k === 0) {
+        res++;
+      }
+    }
+  }
+  return res;
+};
+// 更好的优化方案，是循环一次，然后使用字典计数即可
+// 这个实现也不难，有时间再说
+// @lc code=end
+
---
+++ js/src/2180-countEven.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=2180 lang=javascript
+ *
+ * [2180] 统计各位数字之和为偶数的整数个数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function(num) {
+  // 72 ms, 在所有 JavaScript 提交中击败了25.35%
+  // 辅助函数：判断一个数是否满足各位数字之和为偶数
+  let check = (n) => {
+    let s = String(n);
+    let res = 0;
+    res += Number(s[0]);
+    if (s[1]) {
+      res += Number(s[1]);
+    }
+    if (s[2]) {
+      res += Number(s[2]);
+    }
+    if (s[3]) {
+      res += Number(s[3]);
+    }
+    return res % 2 === 0;
+  };
+
+  let tmp= 0;
+  for (let i = 1; i <= num; i++) {
+    if (check(i)) {
+      tmp++;
+    }
+  }
+  return tmp;
+};
+// @lc code=end
+
