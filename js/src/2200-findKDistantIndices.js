64b6ebc07a906eb10053c0f9a1ba8db946ca5553
Michael An <2331806369@qq.com>
2022-05-12 18:28:05 +0800

    2022-05-12 submit 1

---
+++ js/src/2200-findKDistantIndices.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=2200 lang=javascript
+ *
+ * [2200] 找出数组中的所有 K 近邻下标
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} key
+ * @param {number} k
+ * @return {number[]}
+ * Your runtime beats 9.72 % of javascript submissions
+ * 现在性能不太好，第二次循环有很多重复的
+ */
+var findKDistantIndices = function(nums, key, k) {
+  let tmp = [];
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    if (nums[i] === key) {
+      let middle = i;
+      for (let j = middle - k; j <= middle + k; j++) {
+        if (j >= 0 && j < len) {
+          tmp.push(j);
+        }
+      }
+    }
+  }
+  return Array.from(new Set(tmp));
+};
+// @lc code=end
+
