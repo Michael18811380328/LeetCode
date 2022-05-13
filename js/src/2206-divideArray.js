8dfce165db1a554439e7e5cd268600f79030e297
Michael An <2331806369@qq.com>
2022-05-13 18:24:37 +0800

    2022-05-13 update

---
+++ js/src/2206-divideArray.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=2206 lang=javascript
+ *
+ * [2206] 将数组划分成相等数对
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ * 直接循环一次，然后记录出现的次数，最后次数都是偶数即可
+ * Your runtime beats 42.16 % of javascript submissions
+ */
+var divideArray = function(nums) {
+  let dict = {};
+  nums.forEach(item => {
+    if (!dict[item]) {
+      dict[item] = true;
+    } else {
+      delete dict[item];
+    }
+  });
+  return Object.keys(dict).length === 0;
+  // console.log(divideArray([3,2,3,2,2,2]));
+  // console.log(divideArray([1,2,3,4]));
+};
+// @lc code=end
+
