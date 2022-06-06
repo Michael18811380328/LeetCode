001009c7ca93f05b68bb78c0bd24e98c898221f1
Michael An <1822852997@qq.com>
2022-06-06 22:24:05 +0800

    add issues

---
+++ js/src/2235-sum.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=2235 lang=javascript
+ *
+ * [2235] 两整数相加
+ */
+
+// @lc code=start
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number} number
+ * Your runtime beats 95.76 % of javascript submissions
+ */
+var sum = function(num1, num2) {
+  return num1 + num2;
+};
+// @lc code=end
+
---
+++ js/src/2236-checkTree.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=2236 lang=javascript
+ *
+ * [2236] 判断根结点是否等于子结点之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ * Your runtime beats 94.47 % of javascript submissions
+ */
+var checkTree = function(root) {
+  return root.val === (root.left.val + root.right.val);
+};
+// @lc code=end
+
