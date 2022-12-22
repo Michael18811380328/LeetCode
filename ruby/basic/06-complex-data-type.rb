8890eed3d2544903b0cc688f82faeacb7a5686ee
Michael An <2331806369@qq.com>
2022-12-22 12:03:32 +0800

    add ruby files

--- ruby/basic/06-complex-data-type.rb
+++ ruby/basic/06-complex-data-type.rb
@@ -1,18 +1,24 @@
 # coding=utf-8
 # Ruby 的复杂数据类型：Array/Hash(Rush哈希类似于JS对象)/范围
-# 这里简单做一个使用，后面说具体的操作
 
+# array
 arr = [10, 0.13, "hello"];
 arr.each do |i|
   puts i
 end
 
-hsh = colors = { "red" => "ff0000", "green" => "00ff00", "blue" => "0000ff" };
-hsh.each  do |key, value|
+# hash
+hsh = colors = {
+    "red" => "ff0000",
+    "green" => "00ff00",
+    "blue" => "0000ff",
+};
+
+hsh.each do |key, value|
   print key, " is ", value, "\n";
 end
 
-# 范围类型
+# range
 (10..20).each do |n|
   print n, " \n";
 end
