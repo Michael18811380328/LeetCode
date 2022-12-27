f6d34dc3453219762b6e9ce08c93391b4357779e
Michael An <2331806369@qq.com>
2022-12-27 22:15:24 +0800

    add

--- todo/exercise/2022-12.js
+++ todo/exercise/2022-12.js
@@ -109,3 +109,47 @@ const monotoneIncreasingDigits = function(n) {
   return n;
 }
 
+// 127 广度优先遍历（无向图）
+const ladderLength = function(beginWord, endWord, wordList) {
+  canConvert = (a, b) => {
+    let num = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        num++;
+      }
+      if (num > 1) {
+        return false;
+      }
+    }
+    return true;
+  };
+  // 去重
+  const index = wordList.indexOf(beginWord);
+  if (index > -1) {
+    wordList.splice(index, 1);
+  }
+  const list = [];
+  list.push(beginWord);
+  // 初始化的长度是2
+  let times = 2;
+  while (list.length > 0) {
+    let currentLen = list.length;
+    while (currentLen) {
+      currentLen--;
+      for (let i = 0; i < wordList.length; i++) {
+        if (canConvert(item, wordList[i])) {
+          if (wordList[i] === endWord) {
+            return times;
+          } else {
+            list.push(wordList[i]);
+            wordList.splice(i, 1);
+            i--;
+          }
+        }
+      }
+    }
+    times++;
+  }
+  return 0;
+}
+
