262f466b17096cb8f7548517aa90e36356ccb1e0
Michael An <2331806369@qq.com>
2023-04-22 17:11:20 +0800

    2023-04-22

---
+++ python/src/2639-findColumnWidth.py
@@ -0,0 +1,11 @@
+class Solution:
+  def findColumnWidth(self, grid: List[List[int]]) -> List[int]:
+    result = []
+    for i in range(0, len(grid[0])):
+      tmp = 0
+      for j in range(0, len(grid)):
+        curr = len(str(grid[j][i]))
+        if curr > tmp:
+          tmp = curr
+      result.append(tmp)
+    return result
---
+++ typescript/src/2639-findColumnWidth.ts
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[]}
+ * 简单，遍历矩阵找到最大值
+ */
+// 60 ms, 在所有 JavaScript 提交中击败了92.22%
+function findColumnWidth(grid: number[][]): number[] {
+  const rowLen = grid.length;
+  const columnLen = grid[0].length;
+  const result: number[] = [];
+  for (let i = 0; i < columnLen; i++) {
+    let tmp = 0;
+    for (let j = 0; j < rowLen; j++) {
+      const curr = String(grid[j][i]).length;
+      if (curr > tmp) {
+        tmp = curr;
+      }
+    }
+    result.push(tmp);
+  }
+  return result;
+}
+
+// console.log(findColumnWidth([[1], [22], [333]])); // [3]
+// console.log(
+//   findColumnWidth([
+//     [-15, 1, 3],
+//     [15, 7, 12],
+//     [5, 6, -2],
+//   ])
+// ); // [3,1,2]
+
+export {findColumnWidth};
